70457f0f3f01eb2a57cebad164fb790ec666228c
Abhinav Jonnada <abhinav.jonnada@gmail.com>
2023-09-15 23:58:23 -0400

    add .env variable for gmap key

--- src/main.js
+++ src/main.js
@@ -29,7 +29,7 @@ app.use(router)
 app.use(VueGoogleMaps, {
   load: {
       v: 3.53,
-      key: 'AIzaSyA_lSDE1PTzh4hMnTpyiOuDXASGQHdsR6U',
+      key: process.env.GOOGLE_MAPS_KEY,
       libraries: "places"
   },
 }).mount('#app')
