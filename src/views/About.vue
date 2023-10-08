9f2b0263dd5f6c7dfd8c5911ff8819dbe4563f65
Abhinav Jonnada <abhinav.jonnada@gmail.com>
2023-10-08 03:13:58 -0400

    edits

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,8 +4,7 @@
           <img id="headerImg" alt="Vue logo" src="../assets/logo.png">
           <h1><b>About</b></h1>
           <img id="abtImg" src="https://github.com/abhinavjonnada82/wffawebapp/assets/30497847/341aabf2-ec54-420f-9b0c-7dacf28735b5" href="https://www.freepik.com/free-vector/computer-monitor-graphic-animator-creating-video-game-modeling-motion-processing-video-file-using-professional-editor-vector-illustration-graphic-design-art-designer-workplace-concept_11671412.htm#query=computer%20graphic&position=0&from_view=keyword&track=ais" />
-          <p> Need help with something? submit your issue over <a target="_blank" href="https://form.jotform.com/232801239882156">here </a></p>
-          <br />
+          <h3> Need help with something? Submit your issue <a target="_blank" href="https://form.jotform.com/232801239882156">here. </a></h3>
           <Footer>Made with ❤️ in Wichita | Connect with us on <a target="_blank" href="https://www.facebook.com/WFFAPage">Facebook</a> | WFFA © 2023</Footer>
           <br />
           <br/>
